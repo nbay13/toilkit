379839f361bba41737a8bf9b7c9fe48d45045b6d
williamhuang3 <williamhuang.whh@gmail.com>
2024-01-17 11:23:15 -0800

    fixed bbduk bbdseal refgenome arg

--- toilkit/commands/batch_bbduk_bbseal.py
+++ toilkit/commands/batch_bbduk_bbseal.py
@@ -6,6 +6,7 @@ import glob
 def bbduk_bbseal(args):
     bbduk = args.bbduk_dir + '/bbduk.sh'
     bbseal = args.bbduk_dir + '/seal.sh'
+    trim_galore_adapter_dir = args.bbduk_dir + '/resources/'
     ref_genome_dir = args.ref_genome_dir
     # get filenames
     NAMES = []
@@ -24,7 +25,7 @@ def bbduk_bbseal(args):
             print(f'{p} started at {datetime.datetime.now()}')
             subprocess.run(
                 [bbduk, f'in1={p}_R1.fastq.gz', f'in2={p}_R2.fastq.gz', f'out1={p}_R1.trim.fq', f'out2={p}_R2.trim.fq',
-                 f'ref= {os.path.join(ref_genome_dir, "trim_galore_adapter.fa")}', 'ktrim=r', 'k=10', 'mink=7', 'hdist=1', 'tpe', 'tbo'])
+                 f'ref= {os.path.join(trim_galore_adapter_dir, "trim_galore_adapter.fa")}', 'ktrim=r', 'k=10', 'mink=7', 'hdist=1', 'tpe', 'tbo'])
             subprocess.run(
                 [bbseal, '-Xmx8192m', f'in={p}_R#.trim.fq', f'pattern={p}_out_%_#.fq.gz', f'outu={p}_unmapped_#.fq.gz', 'ambig=all',
                  f'ref={os.path.join(ref_genome_dir,"gencode.v31.transcripts.fa.gz")}, {os.path.join(ref_genome_dir, "gencode.vM22.transcripts.fa.gz")}',
