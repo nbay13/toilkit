fed8de9423b8c77f0a1849b4a1386edb0fa38230
williamhuang3 <williamhuang.whh@gmail.com>
2024-01-31 12:07:37 -0800

    fixd rename toil output

--- toilkit/commands/rename_toil_output.py
+++ toilkit/commands/rename_toil_output.py
@@ -7,6 +7,7 @@ def rename_toil_output(args):
     direction = args.direction
     input_dir = os.path.dirname(input_file)
     with open(input_file, 'r', encoding='utf-8') as file:
+        header = next(file)
         for line in file:
             parts = line.strip().split('\t')
             if len(parts) < 2:
@@ -21,24 +22,33 @@ def rename_toil_output(args):
             new_tar_gz = os.path.join(input_dir, new + ".tar.gz")
             orig_bam = os.path.join(input_dir, orig + ".sortedByCoord.md.bam")
             new_bam = os.path.join(input_dir, new + ".sortedByCoord.md.bam")
-
-            # Extracting tar.gz file
-            with tarfile.open(orig_tar_gz, 'r:gz') as tar:
-                tar.extractall(input_dir)
-
-            # Rename extracted folder
-            os.rename(orig, new)
-
-            # Create new tar.gz from the renamed folder
-            with tarfile.open(new_tar_gz, 'w:gz') as tar:
-                tar.add(new, arcname=os.path.basename(new))
-
-            # Remove the extracted folder
-            shutil.rmtree(os.path.join(input_dir, new))
-
-            # Remove original tar.gz
-            os.remove(orig_tar_gz)
-
-            # Rename bam file
+            
+            if os.path.exists(orig_tar_gz):
+                rename_tar(orig_tar_gz, new_tar_gz, input_dir, orig, new)
             if os.path.exists(orig_bam):
-                os.rename(orig_bam, new_bam)
+                rename_bam(orig_bam, new_bam)
+           
+
+
+
+def rename_tar(orig_tar_gz: str, new_tar_gz: str, input_dir: str, orig, new):
+    # Extracting tar.gz file
+    with tarfile.open(orig_tar_gz, 'r:gz') as tar:
+        tar.extractall(input_dir)
+
+    # Rename extracted folder
+    os.rename(orig, new)
+
+    # Create new tar.gz from the renamed folder
+    with tarfile.open(new_tar_gz, 'w:gz') as tar:
+        tar.add(new, arcname=os.path.basename(new))
+
+    # Remove the extracted folder
+    shutil.rmtree(os.path.join(input_dir, new))
+
+    # Remove original tar.gz
+    os.remove(orig_tar_gz)
+    
+def rename_bam(orig_bam: str, new_bam: str):
+    # Rename bam file
+    os.rename(orig_bam, new_bam)
