a89484fd131588f6602e12b59d25f9f4b29fac93
williamhuang3 <williamhuang.whh@gmail.com>
2024-02-09 21:13:23 -0800

    removed unnecessary files

--- toilkit/commands/reference_py_files/cut_manifest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-manifest_file=$1
-split_num=$2
-manifest_ext='.'"${1##*.}"
-filename="${1%.*}"
-split -d -l $split_num -a 1 $manifest_file $filename'.' --additional-suffix $manifest_ext
